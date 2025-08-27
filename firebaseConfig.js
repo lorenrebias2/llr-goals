04a17d6b8be9fa275241e97986ed06b8a0af5a52
lorenrebias2 <lorenrebias7@gmail.com>
2025-08-27 15:34:42 +0800

    first configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJWprsSUOqwu1MgigpBIxmSOqu-b8gQK0",
+  authDomain: "writeright-f8223.firebaseapp.com",
+  projectId: "writeright-f8223",
+  storageBucket: "writeright-f8223.firebasestorage.app",
+  messagingSenderId: "77820924122",
+  appId: "1:77820924122:web:e7a0b14600e2cbbc2a55e3",
+  measurementId: "G-GR8MWXSTP2"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
